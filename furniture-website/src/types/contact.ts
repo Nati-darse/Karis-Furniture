7069579c6732c69ca846d853db357561499ecace
Nati-darse <natnaeldarsema@gmail.com>
2025-09-17 02:45:06 +0300

    contact types

---
+++ furniture-website/src/types/contact.ts
@@ -0,0 +1,45 @@
+export interface ContactFormData {
+  interest: string;
+  projectDescription: string;
+  timeline: string;
+  budget: string;
+  name: string;
+  email: string;
+  phone: string;
+  address?: string;
+  productName?: string; // For pre-filled product from gallery
+}
+
+export interface FormStepProps {
+  formData: ContactFormData;
+  updateFormData: (data: Partial<ContactFormData>) => void;
+  goToNextStep: () => void;
+  goToPreviousStep: () => void;
+}
+
+export const interestOptions = [
+  'Custom Dining Table',
+  'Living Room Furniture',
+  'Bedroom Set',
+  'Outdoor Furniture',
+  'Office Furniture',
+  'Custom Cabinetry',
+  'Restoration Project',
+  'Other (Please describe in project details)'
+];
+
+export const budgetOptions = [
+  'Under $1,000',
+  '$1,000 - $3,000',
+  '$3,000 - $5,000',
+  '$5,000 - $10,000',
+  '$10,000+'
+];
+
+export const timelineOptions = [
+  'ASAP (Within 1 month)',
+  '1-3 months',
+  '3-6 months',
+  '6+ months',
+  'Just exploring options'
+];
