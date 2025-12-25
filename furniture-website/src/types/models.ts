bbf9d605591a3d2c06c0b2aed4aaf7e88bfac23d
Nati Darse <natnaeldarsema@gmail.com>
2025-12-25 12:08:27 +0300

    model

---
+++ furniture-website/src/types/models.ts
@@ -0,0 +1,21 @@
+export interface Product {
+  id: string;
+  title: string;
+  category: string;
+  description: string;
+  price: number;
+  images: string[];
+  dimensions?: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  materials: string[];
+  featured?: boolean;
+}
+
+export interface ProductFilters {
+  category: string;
+  searchQuery: string;
+}
+
